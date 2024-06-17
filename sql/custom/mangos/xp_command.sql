4a1b066e2ccd25d965a3cd8aba7d31a1fd80f37a
Brian Oost <brianleon77@gmail.com>
2024-06-17 23:41:53 +0200

    Update xp_command.sql

--- sql/custom/mangos/xp_command.sql
+++ sql/custom/mangos/xp_command.sql
@@ -1,1 +1,3 @@
 INSERT INTO command VALUES('xp', '0', 'Syntax: .xp #subcommand XP rate commands');
+INSERT INTO command VALUES('xp set', '0', 'Syntax: .xp set $XpNumber - Modify your experience multiplier.');
+INSERT INTO command VALUES('xp current', '0', 'Syntax: .xp current - Shows current experience multiplier.');
